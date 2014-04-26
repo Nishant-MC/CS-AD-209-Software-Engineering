5ec8b708ac265e3789a204516055ccf8253d99e5
Nishant-MC <nm1345@nyu.edu>
2014-04-26 16:20:12 +0400

    Adding-poetry-server-still-buggy

---
+++ Homework-4-poetry-server/Audience.py
@@ -0,0 +1,78 @@
+import os
+import json
+import logging
+
+from twisted.internet import reactor, defer
+
+from stompest.async import Stomp
+from stompest.async.listener import SubscriptionListener
+from stompest.config import StompConfig
+from stompest.protocol import StompSpec
+
+
+class Audience(object):
+
+    QUEUE = '/queue/poems'
+    ERROR_QUEUE = '/queue/poems/error'
+
+    def __init__(self, config=None):
+        if config is None:
+            address = os.getenv('ACTIVEMQ_PORT_61616_TCP', 'tcp://localhost:61616')
+            config = StompConfig(address)
+        self.config = config
+        self.POETRY = {}
+
+    @defer.inlineCallbacks
+    def run(self):
+        client = yield Stomp(self.config).connect()
+        headers = {
+            # client-individual mode is necessary for concurrent processing
+            # (requires ActiveMQ >= 5.2)
+            StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL,
+            # the maximal number of messages the broker will let you work on at the same time
+            'activemq.prefetchSize': '100',
+        }
+        client.subscribe(self.QUEUE, headers, listener=SubscriptionListener(self.consume, errorDestination=self.ERROR_QUEUE))
+
+    def getTitle(self, message):
+        self.POETRY[message["name"]]["count"] = message["length"]
+
+    def getLine(self, message):
+        if (self.data["type"] != "title"):
+            name = message["name"]
+            self.POETRY[name]["length"].append(message)
+        else:
+            self.getTitle(data)
+
+    def sitRep(self, name):
+        poem = self.POETRY[name]
+        if ("count" not in poem or poem["count"] > len(poem["length"])):
+            print "I have %d lines of poetry from %s" % (len(poem["length"]), name)
+        else:
+            self.printPoem(poem)
+
+    def printPoetry(self, poem):
+        lines = sorted(poem["length"], key=lambda msg: msg['line'])
+        print "This is from: %s" % poem["name"]
+        for line in lines:
+            print line["content"]
+
+    def consumer(self, client, frame):
+        data = json.loads(frame.body)
+        name = data["name"]
+
+        if name not in self.POETRY:
+            self.POETRY[name] = {
+                    "name": name,
+                    "length": []
+            }
+
+        self.getLine(data)
+        self.sitRep(name)
+
+def main():
+    Audience().run()
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
---
+++ Homework-4-poetry-server/Poet.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import json
+import logging
+import optparse
+
+from twisted.internet import defer, reactor
+from stompest.config import StompConfig
+from stompest.async import Stomp
+from stompest.async.listener import ReceiptListener
+
+def parse_args():
+    usage = """
+usage: %prog poetry-file
+python Poet.py <poem.txt>
+            """
+    parser = optparse.OptionParser(usage)
+    options, args = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error('Only one poem at a time please!')
+
+    poem = args[0]
+
+    if os.path.exists(args[0]) == False:
+        parser.error('%s does not exist!' % poetry_file)
+
+    return options, poem
+
+
+class Poet(object):
+
+    QUEUE = '/queue/poems'
+
+    def __init__(self, name, text, config=None):
+        if config is None:
+            address = os.getenv('ACTIVEMQ_PORT_61616_TCP', 'tcp://localhost:61616')
+            print "%s will receive a line of poetry." % address
+            config = StompConfig(address)
+        self.config = config
+        self.name = name
+        self.text = text.split('\n')
+
+    @defer.inlineCallbacks
+
+    def run(self):
+        client = yield Stomp(self.config).connect()
+        client.add(ReceiptListener(1.0))
+
+        x = 0
+        yield client.send(self.queue, json.dumps({'type': 'title', 'name': self.name, 'length': len(self.text)}), receipt='message-%d' % x)
+        for line in self.text:
+            x += 1
+            yield client.send(self.QUEUE,
+                              
+                              json.dumps({'type': 'line',
+                                          'name': self.name,
+                                          'line': x,
+                                          'content': line}),
+
+                              receipt='message-%d' % x)
+
+        client.disconnect(receipt='bye')
+        yield client.disconnected
+        reactor.stop()
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    Poet().run()
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
